3faa0f0c9e4650a76f3e106602a8a375de5b2d6c
Kimdogyun1997 <122517647+Kimdogyun1997@users.noreply.github.com>
2023-01-13 18:02:03 +0900

    Add files via upload

---
+++ day02_homework2.py
@@ -0,0 +1,18 @@
+#2
+
+
+small=True
+green=True
+
+
+if small==True:
+    if green==True:
+        print('완두콩은 작고 녹색이다')
+    else:
+        print('체리는 작고 녹색이 아니다')
+else:
+    if green==True:
+        print('수박은 크고 녹색이다')
+    else:
+        print('호박은 크고 녹색이 아니다')
+
